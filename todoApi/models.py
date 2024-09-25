4faf4d10e4e6a09c9059010dae6d7627e83fe3b5
Ved Jha <vedprakash@fanclash.in>
2024-09-25 20:08:40 +0530

    modified model and removed priority column

--- todoApi/models.py
+++ todoApi/models.py
@@ -10,7 +10,7 @@ class Todos(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     description = Column(String)
-    priority = Column(Integer)
+    # priority = Column(Integer)
     complete = Column(Boolean, default=True)
 
 
